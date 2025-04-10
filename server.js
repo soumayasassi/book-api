91ce9cc31afbbc98fd06d100c6d3cb9f3704c66c
soumayasassi <soumaya.sassimahfoudh@esprit.tn>
2025-04-10 15:50:31 +0100

    server.js modif

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
 
 app.use('/api/books', bookRoutes);
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.get('/', (req, res) => {
+  res.send('Bienvenue sur l\'API Book. Accédez à /api-docs pour la documentation.');
+})
 mongoose.set("strictQuery", false);
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
