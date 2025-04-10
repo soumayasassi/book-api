364b65f59d7469f565ca1fb85c608159ae4a5564
soumayasassi <soumaya.sassimahfoudh@esprit.tn>
2025-04-10 13:18:32 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bookRoutes = require('./routes/books');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger/swagger.json');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+app.use('/api/books', bookRoutes);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
