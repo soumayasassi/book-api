500345f4113e8962f4c86eb1d1e448df1b705904
soumayasassi <soumaya.sassimahfoudh@esprit.tn>
2025-04-11 11:52:09 +0100

    change server config

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 
-app.use('/api/books', bookRoutes);
+app.use('/books', bookRoutes);
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.get('/', (req, res) => {
   res.send('Bienvenue sur l\'API Book. Accédez à /api-docs pour la documentation.');
