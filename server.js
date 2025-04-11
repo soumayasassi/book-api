9b211b62cd55d088b8d3639156b33fe5456f0c91
soumayasassi <soumaya.sassimahfoudh@esprit.tn>
2025-04-11 16:05:41 +0100

    corrige cluster

--- server.js
+++ server.js
@@ -16,13 +16,13 @@ app.use(express.json());
 app.use('/books', bookRoutes);
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.get('/', (req, res) => {
-  res.send('Bienvenue sur l\'API Book. Accédez à /api-docs pour la documentation.');
+  res.send('Bienvenue sur l\'API Book. Accédez à /docs pour la documentation.');
 })
 mongoose.set("strictQuery", false);
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  family: 4 // 👈 force l'utilisation d'IPv4
+  family: 4 
 })
 
 
