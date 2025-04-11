3515f609d86cc6a7938c71c5fd13a4669bb271e5
soumayasassi <soumaya.sassimahfoudh@esprit.tn>
2025-04-11 16:39:00 +0100

    error

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const swaggerDocument = require('./swagger/swagger.json');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
