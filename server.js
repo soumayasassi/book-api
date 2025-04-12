e6a7941045d33912eb9fd1290f4492bfba39b228
soumayasassi <soumaya.sassimahfoudh@esprit.tn>
2025-04-12 09:49:33 +0100

    new trial

--- server.js
+++ server.js
@@ -24,7 +24,8 @@ mongoose.connect(process.env.MONGO_URI, {
   useUnifiedTopology: true,
   family: 4 
 })
-
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Error connecting to MongoDB:', err));
 
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
